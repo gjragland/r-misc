cde58a123251b19131734647d0db5a0db82f9514
gjragland <gragland@ksu.edu>
2016-03-23 14:55:34 -0500

    Update misc.r

--- misc.r
+++ misc.r
@@ -9,3 +9,9 @@ par(bg='black',fg='white')
 plot(data$ltrans,data$atrans,col.lab='white',col.axis='white')
 abline(fm,col='white')
 
+#change libpath to install in root-access directory (make library available to all users)
+#for lab linux machine example
+.libPaths("/usr/lib/R/library")
+install.packages()
+
+
